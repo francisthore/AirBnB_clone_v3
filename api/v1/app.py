5aff5ab3d0de1e4a5d17727807947cf7b5fa7e63
Francis Thore <thorefrancis@gmail.com>
2024-06-07 21:40:51 +0200

    pycode style

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,15 +10,18 @@ from models import storage
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.errorhandler(404)
 def not_found(error):
     """JSON formatted 404 response"""
     return jsonify({"error": "Not found"}), 404
 
+
 @app.teardown_appcontext
 def close_db(error):
     """Close storage on teardown"""
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
