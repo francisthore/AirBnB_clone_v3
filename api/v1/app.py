a6eb4869af9e0acf84d633dbeabb7cae2fe84eed
Wandzfuture <michael.zabas@gmail.com>
2024-06-09 12:55:14 +0000

    0x05. AirBnB clone - RESTful API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
 """
-This script sets up a Flask web server.
+Main application module
 """
-import os
-from flask import Flask, jsonify
 from api.v1.views import app_views
+from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
+from os import getenv
 
 app = Flask(__name__)
+
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 app.register_blueprint(app_views)
 
 
-@app.errorhandler(404)
-def not_found(error):
-    """JSON formatted 404 response"""
-    return jsonify({"error": "Not found"}), 404
-
-
 @app.teardown_appcontext
-def close_db(error):
-    """Close storage on teardown"""
+def close_storage(exception):
+    """
+    Close storage connection when app context is torn down
+    """
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found_error(exception):
+    """
+    Handle 404 errors by returning a JSON response
+    """
+    error_response = {"error": "Not found"}
+    return jsonify(error_response), 404
+
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', '5000')
-    app.run(host=host, port=port, threaded=True)
+    app.run(getenv("HBNB_API_HOST", "0.0.0.0"),
+            getenv("HBNB_API_PORT", "5000"))
