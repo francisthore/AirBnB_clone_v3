ae88f3828f7215a292e46cd4805595086126e303
Francis Thore <thorefrancis@gmail.com>
2024-06-09 14:58:05 +0200

    places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,6 +8,7 @@ from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
 from models.city import City
+from models.user import User
 from models.place import Place
 from markupsafe import escape
 
